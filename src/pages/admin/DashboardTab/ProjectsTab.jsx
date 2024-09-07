00b62bbc23d7c8e70b51ecca0656af45b9ab7313
FozleRabbi1 <fozlerabbishuvo@gmail.com>
2024-09-07 15:44:04 +0600

    working conditional table data

--- src/pages/admin/DashboardTab/ProjectsTab.jsx
+++ src/pages/admin/DashboardTab/ProjectsTab.jsx
@@ -1,13 +1,28 @@
 /* eslint-disable react/no-unescaped-entities */
 
-import { Table } from "antd";
+import { Button, Modal, Table } from "antd";
 import { ProjectsApi } from "../../../redux/fetures/prjects/ProjectsApi";
+import moment from "moment";
+import { useState } from "react";
+import { FaRegEdit, FaRegStar, FaStar } from "react-icons/fa";
+import { AiOutlineMessage } from "react-icons/ai";
 
 
 const ProjectsTab = () => {
     const { data, isLoading } = ProjectsApi.useGetAllProjectsQuery({})
+    const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+    // State to track active stars
+    const [activeStars, setActiveStars] = useState([]);
 
-    const tableData = data?.data?.map(({ _id,id, title, users, clients, status, priority, budget, tags, createdAt, updatedAt }) => ({
+    // Handle star click
+    const handleStarClick = (key) => {
+        setActiveStars(prevActiveStars =>
+            prevActiveStars.includes(key)
+                ? prevActiveStars.filter(starKey => starKey !== key)
+                : [...prevActiveStars, key]
+        );
+    };
+    const tableData = data?.data?.map(({ _id, id, title, users, clients, status, priority, budget, tags, createdAt, updatedAt }) => ({
         key: _id,
         id,
         title,
@@ -19,72 +34,160 @@ const ProjectsTab = () => {
         tags,
         createdAt,
         updatedAt,
-      }));
-    
-      const columns = [
-        {title : "Id", dataIndex : "id"},
+    }));
+
+    const onSelectChange = (newSelectedRowKeys) => {
+        setSelectedRowKeys(newSelectedRowKeys);
+    };
+
+    const rowSelection = {
+        selectedRowKeys,
+        onChange: onSelectChange,
+    };
+
+    const titleStyle = { fontWeight: '600', color: '#6b7260', textTransform: 'uppercase' };
+    const [open, setOpen] = useState(false);
+
+    const columns = [
+        { title: <span style={titleStyle}>Id</span>, dataIndex: "id" },
+
         {
-          title: "Title",
-          dataIndex: "title",
+            title: <span style={titleStyle}>Title</span>,
+            dataIndex: "title",
+            render: (title, record) => (
+                <span className="text-blue-600 opacity-90 text-[16px] font-semibold flex items-center">
+                    {title}
+                    {activeStars.includes(record.key) ? (
+                        <FaStar
+                            onClick={() => handleStarClick(record.key)}
+                            className="text-yellow-500 mx-2 cursor-pointer"
+                        />
+                    ) : (
+                        <FaRegStar
+                            onClick={() => handleStarClick(record.key)}
+                            className="text-yellow-500 mx-2 cursor-pointer"
+                        />
+                    )}
+                    <AiOutlineMessage className="text-red-500" />
+                </span>
+            ),
         },
         {
-            title: "Users",
+            title: <span style={titleStyle}>users</span>,
             dataIndex: "users",
             render: (users) => (
-              <div style={{ display: 'flex', gap: '8px' }}>
-                {users.map((url, index) => (
-                  <img key={index} src={url} alt={`User ${index + 1}`} style={{ width: '40px', height: '40px', borderRadius: '50%' }} />
-                ))}
-              </div>
+                <div style={{ display: 'flex', gap: '8px' }} className="flex items-center">
+                    {users.map((url, index) => (
+                        <img key={index} src={url} alt={`User ${index + 1}`} style={{ width: '30px', height: '30px', borderRadius: '50%' }} />
+
+
+                    ))}
+                    <Button onClick={() => setOpen(true)} className="border rounded-full flex justify-center items-center w-[30px] h-[30px] border-blue-600 ml-2 p-[6px] ">
+                        <FaRegEdit className="text-xl text-blue-600" />
+                    </Button>
+                </div>
             ),
-          },
-          {
-            title: "Clients",
+        },
+        {
+            title: <span style={titleStyle}>Clients</span>,
             dataIndex: "clients",
             render: (clients) => (
-              <div style={{ display: 'flex', gap: '8px' }}>
-                {clients.map((url, index) => (
-                  <img key={index} src={url} alt={`Client ${index + 1}`} style={{ width: '40px', height: '40px', borderRadius: '50%' }} />
-                ))}
-              </div>
+                <div style={{ display: 'flex', gap: '8px' }}>
+                    {
+                        clients?.length !== 0 ?
+                            <div className="flex items-center">
+                                {clients.map((url, index) => (
+                                    <img key={index} src={url} alt={`Client ${index + 1}`} style={{ width: '30px', height: '30px', borderRadius: '50%' }} />
+                                ))}
+                                <Button onClick={() => setOpen(true)} className="border rounded-full flex justify-center items-center w-[30px] h-[30px] border-blue-600 ml-2 p-[6px] ">
+                                    <FaRegEdit className="text-xl text-blue-600" />
+                                </Button>
+
+                            </div>
+                            :
+                            <div className="flex items-center">
+                                <h2 className="bg-blue-600 text-white font-medium px-2 rounded-md uppercase opacity-80 flex items-center "> Not Assigned </h2>
+                                <Button onClick={() => setOpen(true)} className="border rounded-full flex justify-center items-center w-[30px] h-[30px] border-blue-600 ml-2 p-[6px] ">
+                                    <FaRegEdit className="text-xl text-blue-600" />
+                                </Button>
+                            </div>
+                    }
+                </div>
             ),
-          },
-        {
-          title: "Status",
-          dataIndex: "status",
         },
         {
-          title: "Priority",
-          dataIndex: "priority",
+            title: <span style={titleStyle}>Status</span>,
+            dataIndex: "status",
         },
         {
-          title: "Budget",
-          dataIndex: "budget",
+            title: <span style={titleStyle}>Priority</span>,
+            dataIndex: "priority",
         },
         {
-          title: "Tags",
-          dataIndex: "tags",
+            title: <span style={titleStyle}>Budget</span>,
+            dataIndex: "budget",
         },
         {
-          title: "Created At",
-          dataIndex: "createdAt",
+            title: <span style={titleStyle}>Tags</span>,
+            dataIndex: "tags",
         },
         {
-          title: "Updated At",
-          dataIndex: "updatedAt",
+            title: <span style={titleStyle}>Starts At</span>,
+            dataIndex: "createdAt",
+            render: (date) => moment(date).format('MMMM DD, YYYY'),
         },
-      ];
+        {
+            title: <span style={titleStyle}>Ends at</span>,
+            dataIndex: "updatedAt",
+            render: (date) => moment(date).format('MMMM DD, YYYY'),
+        },
+    ];
 
     return (
         <div>
-            <h2 className="text-2xl text-gray-500 font-bold">Admin's Projects</h2>
+            <h2 className="text-2xl text-gray-500 font-bold mb-4">Admin's Projects</h2>
 
             <Table
+                bordered
                 loading={isLoading}
                 columns={columns}
                 dataSource={tableData}
                 showSorterTooltip={{ target: "sorter-icon" }}
+                rowSelection={rowSelection}
+
             />
+
+            <Modal
+                title="Up Comming"
+                centered
+                open={open}
+                onOk={() => setOpen(false)}
+                onCancel={() => setOpen(false)}
+                width={1000}
+            >
+                <p>some contents...</p>
+                <p>some contents...</p>
+                <p>some contents...</p>
+                <p>some contents...</p>
+                <p>some contents...</p>
+                <p>some contents...</p>
+                <p>some contents...</p>
+                <p>some contents...</p>
+                <p>some contents...</p>
+                <p>some contents...</p>
+                <p>some contents...</p>
+                <p>some contents...</p>
+                <p>some contents...</p>
+                <p>some contents...</p>
+                <p>some contents...</p>
+                <p>some contents...</p>
+                <p>some contents...</p>
+                <p>some contents...</p>
+                <p>some contents...</p>
+                <p>some contents...</p>
+                <p>some contents...</p>
+                <p>some contents...</p>
+            </Modal>
 
 
         </div>
