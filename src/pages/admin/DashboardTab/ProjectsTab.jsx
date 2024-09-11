117f0c970b5b93a81347002a4e1c8cc4afab650b
FozleRabbi1 <fozlerabbishuvo@gmail.com>
2024-09-11 12:50:16 +0600

    little update

--- src/pages/admin/DashboardTab/ProjectsTab.jsx
+++ src/pages/admin/DashboardTab/ProjectsTab.jsx
@@ -12,7 +12,7 @@ import { CiCircleInfo } from "react-icons/ci";
 import TSForm from "../../../components/form/TSForm";
 import TSInput from "../../../components/form/TSInput";
 import TSSelect from "../../../components/form/TSSelect";
-import { tagsArray } from "../../../constant/constant";
+import { tagsArray, tagStyles } from "../../../constant/constant";
 
 const ProjectsTab = () => {
 
@@ -29,9 +29,7 @@ const ProjectsTab = () => {
     const [deleteProject,] = ProjectsApi.useDeleteProjectMutation()
     const [modalData, setModalData] = useState({})
 
-    const uniqueTags = [...new Set(data?.data?.flatMap(item => item.tags))];
-    console.log(uniqueTags);
-    
+    // const uniqueTags = [...new Set(data?.data?.flatMap(item => item.tags))];
 
 
     const handleStarClick = async (key, i) => {
@@ -62,7 +60,7 @@ const ProjectsTab = () => {
         label: item,
     }));
 
-    
+
     const tableData = data?.data?.map(({ _id, id, title, users, clients, status, priority, budget, tags, createdAt, updatedAt, isFavourite }) => ({
         key: _id,
         _id,
@@ -171,6 +169,22 @@ const ProjectsTab = () => {
             _id);
 
 
+    }
+
+    const singleDataDelete = (id) => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: `You won't remove This User`,
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                deleteProject({ idArray: [id] });
+            }
+        });
     }
 
     const columns = [
@@ -290,47 +304,10 @@ const ProjectsTab = () => {
             render: (tags) => (
                 <div className="">
                     <span className="text-gray-500 opacity-90 text-[15px] font-semibold flex items-center">
-
                         {tags.map((item, index) => (
                             <span
                                 key={index}
-                                className={`mr-2 border rounded px-1 ${item === "Cloud Computing" ? "text-red-500 bg-red-100" :
-                                    item === "Cloud Computing" ? "text-red-500 bg-red-100" :
-                                        item === "Solutions" ? "text-green-500 bg-green-100" :
-                                            item === "IT" ? "text-yellow-500 bg-yellow-100" :
-                                                item === "SEO" ? "text-sky-500 bg-sky-100" :
-                                                    item === "Software Integration" ? "text-purple-500 bg-purple-100" :
-                                                        item === "Technology" ? "text-blue-500 bg-blue-100" :
-                                                            item === "Business" ? "text-orange-500 bg-orange-100" :
-                                                                item === "Web Development" ? "text-indigo-500 bg-indigo-100" :
-                                                                    item === "UI/UX" ? "text-pink-500 bg-pink-100" :
-                                                                        item === "Redesign" ? "text-teal-500 bg-teal-100" :
-                                                                            item === "E-commerce" ? "text-emerald-500 bg-emerald-100" :
-                                                                                item === "Platform" ? "text-cyan-500 bg-cyan-100" :
-                                                                                    item === "Development" ? "text-lime-500 bg-lime-100" :
-                                                                                        item === "App Development" ? "text-rose-500 bg-rose-100" :
-                                                                                            item === "iOS" ? "text-violet-500 bg-violet-100" :
-                                                                                                item === "Android" ? "text-amber-500 bg-amber-100" :
-                                                                                                    item === "Digital Marketing" ? "text-fuchsia-500 bg-fuchsia-100" :
-                                                                                                        item === "Strategy" ? "text-gray-500 bg-gray-100" :
-                                                                                                            item === "API" ? "text-blue-600 bg-blue-100" :
-                                                                                                                item === "Backend" ? "text-gray-600 bg-gray-100" :
-                                                                                                                    item === "Network Security" ? "text-green-600 bg-green-100" :
-                                                                                                                        item === "Audit" ? "text-orange-600 bg-orange-100" :
-                                                                                                                            item === "CRM" ? "text-teal-600 bg-teal-100" :
-                                                                                                                                item === "Implementation" ? "text-purple-600 bg-purple-100" :
-                                                                                                                                    item === "Cloud" ? "text-blue-400 bg-blue-100" :
-                                                                                                                                        item === "Migration" ? "text-red-400 bg-red-100" :
-                                                                                                                                            item === "Social Media" ? "text-indigo-400 bg-indigo-100" :
-                                                                                                                                                item === "Management" ? "text-green-400 bg-green-100" :
-                                                                                                                                                    item === "Mobile App" ? "text-orange-400 bg-orange-100" :
-                                                                                                                                                        item === "Optimization" ? "text-yellow-400 bg-yellow-100" :
-                                                                                                                                                            item === "Marketing" ? "text-pink-400 bg-pink-100" :
-                                                                                                                                                                item === "Design" ? "text-purple-400 bg-purple-100" :
-                                                                                                                                                                    item === "Content" ? "text-blue-400 bg-blue-100" :
-                                                                                                                                                                        item === "CMS" ? "text-green-500 bg-green-100" :
-                                                                                                                                                                            ""
-                                    }`}
+                                className={`mr-2 border rounded px-1 ${tagStyles[item] || ""}`}
                             >
                                 {item}
                             </span>
@@ -367,18 +344,20 @@ const ProjectsTab = () => {
             ),
         },
         {
-            title: <span style={titleStyle}> Action </span>,
+            title: <span style={titleStyle}>Action</span>,
             dataIndex: "action",
-            render: () => (
+            render: (text, record) => (
                 <div className="">
-                    <button title="Update"><FaEdit className="text-xl mr-6 text-blue-500 " /></button>
-                    <button title="Delete"><RiDeleteBin5Line className="text-xl mr-6 text-red-500 " /></button>
-                    <button title="Duplicate"><RiFoldersLine className="text-xl mr-6 text-yellow-500 " /></button>
-                    <button title="Quick View"><CiCircleInfo className="text-xl text-blue-600 " /></button>
-
+                    <button title="Update"><FaEdit className="text-xl mr-6 text-blue-500" /></button>
+                    <button onClick={() => singleDataDelete(record.key)} title="Delete">
+                        <RiDeleteBin5Line className="text-xl mr-6 text-red-500" />
+                    </button>
+                    <button title="Duplicate"><RiFoldersLine className="text-xl mr-6 text-yellow-500" /></button>
+                    <button title="Quick View"><CiCircleInfo className="text-xl text-blue-600" /></button>
                 </div>
             ),
-        },
+        }
+
     ];
 
     const handleDelete = () => {
@@ -395,13 +374,13 @@ const ProjectsTab = () => {
                 deleteProject({ idArray: ids });
             }
         });
-
     }
 
     const onSubmit = async (data) => {
         console.log(data);
 
     };
+
     const options = [];
     for (let i = 10; i < 36; i++) {
         options.push({
@@ -409,6 +388,7 @@ const ProjectsTab = () => {
             value: i.toString(36) + i,
         });
     }
+
     const handleChange = (value) => {
         console.log(`selected ${value}`);
     };
